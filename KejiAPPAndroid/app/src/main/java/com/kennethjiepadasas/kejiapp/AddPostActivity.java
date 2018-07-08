091682e084cc53cae23b7dac55f85fc31ec25660
Unknown <kennethjiepadasas@gmail.com>
2018-07-08 16:14:25 +0800

    initial commit

---
+++ KejiAPPAndroid/app/src/main/java/com/kennethjiepadasas/kejiapp/AddPostActivity.java
@@ -0,0 +1,13 @@
+package com.kennethjiepadasas.kejiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_post);
+    }
+}
